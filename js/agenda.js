a0cd658e52fd3f9851ff6ef031ef71d24507c515
Ponpes Salafiyah Kalipare <ponpessalafiyahkalipare@gmail.com>
2024-09-03 07:10:00 +0700

    initial commit

--- js/agenda.js
+++ js/agenda.js
@@ -1,1 +1,9 @@
-
+$.ajax({
+  url : 'https://conn.alisita.my.id/hosting/salafiyahkalipare/public/agenda',
+  method : 'post',
+  data : {
+    'open' : 'agenda'
+  },
+  beforeSend: function(){$('#show').html('<div class="spinner-border text-secondary" role="status"><span class="visually-hidden">Loading...</span></div>');},
+  success : function(load){$('#show').html(load);}
+});
